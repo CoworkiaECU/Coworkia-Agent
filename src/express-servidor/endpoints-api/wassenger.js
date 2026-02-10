08c67a4de27427f885da34651c9b1cdb4287c3c9
Tu Nombre <tu-email@ejemplo.com>
2026-02-10 09:27:09 -0500

    fix: eliminar import duplicado normalizeAgentName (bloqueaba servidor)

--- src/express-servidor/endpoints-api/wassenger.js
+++ src/express-servidor/endpoints-api/wassenger.js
@@ -30,7 +30,6 @@ import { getUserLanguage, detectLanguageCommand, getLanguageChangeConfirmation }
 import { processMessage as splitLongMessage, cleanPromptMarkers } from '../../utils/message-splitter.js';
 import { getAgentForm, saveAgentForm, clearAgentForm, getAllUserForms } from '../../servicios/agent-form-manager.js';
 import { normalizeAgentName } from '../../utils/agent-normalizer.js';
-import { normalizeAgentName } from '../../utils/agent-normalizer.js';
 
 // 🆕 NUEVO SISTEMA V2 - Handoffs unificados
 import { resolveIntent, decideResponder, logIntent, INTENT_TYPES } from '../../deteccion-intenciones/intent-resolver-v2.js';
